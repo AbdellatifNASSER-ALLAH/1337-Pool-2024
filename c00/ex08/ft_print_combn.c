917f46f81f773ea04eac6f13a1d20e0166ec9502
Abdellatif Nasser-Allah <nasserallahabdellatif2@gmail.com>
2024-07-01 10:38:24 +0100

    c02 ex00

---
+++ c00/ex08/ft_print_combn.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 00:00:35 by abdnasse          #+#    #+#             */
+/*   Updated: 2024/07/01 00:11:23 by abdnasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+	}
+	else
+	{
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			nb = nb * -1;
+		}
+		if (nb >= 0 && nb < 10)
+		{
+			ft_putchar(nb + '0');
+		}
+		else
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	int	i;
+	int	j;
+
+
+}
