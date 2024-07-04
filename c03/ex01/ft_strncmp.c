bb0a912bf16489f131a803bd0f36a80309e2697b
Abdellatif NASSER-ALLAH <102266455+AbdellatifNASSER-ALLAH@users.noreply.github.com>
2024-07-04 18:12:49 +0100

    Update ft_strncmp.c

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -15,9 +15,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	if (i < n)
-	{
-	while  (s1[i] || s2[i])
+	while  (i < n && (s1[i] || s2[i]))
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
