4c3250c38886bc1f37e254ba09f17cb2d394ff80
Abdellatif Nasser-Allah <nasserallahabdellatif2@gmail.com>
2024-07-18 03:20:38 +0100

    updated

--- c09/ex02/ft_split.c
+++ c09/ex02/ft_split.c
@@ -5,14 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/17 01:33:30 by abdnasse          #+#    #+#             */
-/*   Updated: 2024/07/18 02:10:53 by abdnasse         ###   ########.fr       */
+/*   Created: 2024/07/14 14:16:22 by abdnasse          #+#    #+#             */
+/*   Updated: 2024/07/18 03:20:24 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	ft_is_in(char *charset, char c)
+int	is_sep(char c, char *charset)
 {
 	while (*charset)
 	{
@@ -23,62 +23,78 @@ int	ft_is_in(char *charset, char c)
 	return (0);
 }
 
-int	ft_count_words(char *str, char *sep)
+int	ft_size(char *str, char *sep)
 {
 	int	count;
-	int	i;
 
-	i = 0;
 	count = 0;
-	while (str[i] && str)
+	while (*str)
 	{
-		while (str[i] && ft_is_in(sep, str[i]))
-			i++;
-		if (str[i] && !ft_is_in(sep, str[i]))
+		while (*str && is_sep(*str, sep))
+			str++;
+		if (*str)
 			count++;
-		i++;
-		while (str[i] && !ft_is_in(sep, str[i]))
-			i++;
+		while (*str && !is_sep(*str, sep))
+			str++;
 	}
 	return (count);
 }
 
-void	ft_to_arr(char **p, int size, char *str, char *sep)
+int	ft_appand(char *word, char *str, int j, char *sep)
 {
-	int	i;
-	int	j;
-	int	k;
-	int	sub;
+	int		i;
 
 	i = 0;
-	j = 0;
-	while (i < size)
+	while (str[j] && !is_sep(str[j], sep))
+		word[i++] = str[j++];
+	word[i] = '\0';
+	return (j);
+}
+
+char	*ft_strdup(char *str, int *index, char *sep, int slen)
+{
+	char	*word;
+	int		i;
+	int		len;
+
+	i = *index;
+	len = 0;
+	while (str[i] && is_sep(str[i], sep))
+		i++;
+	*index = i;
+	while (str[i] && !is_sep(str[i], sep))
 	{
-		while (str[j] && ft_is_in(sep, str[j]))
-			j++;
-		sub = j;
-		while (str[j] && !ft_is_in(sep, str[j]))
-			j++;
-		p[i] = (char *)malloc((j - sub + 1) * sizeof(char));
-		k = 0;
-		j = sub;
-		while (str[j] && !ft_is_in(sep, str[j]))
-			p[i][k++] = str[j++];
-		p[i][k] = '\0';
+		len++;
 		i++;
 	}
-	p[i] = 0;
+	len = len + (slen * 2) + 1;
+	word = (char *)malloc(len * sizeof(char));
+	*index = ft_appand(word, str, *index, sep);
+	return (word);
 }
 
 char	**ft_split(char *str, char *charset)
 {
-	char	**ptr;
+	char	**p;
 	int		size;
+	int		slen;
+	int		i;
+	int		index;
 
-	size = ft_count_words(str, charset);
-	ptr = (char **)malloc((size + 1) * sizeof(char *));
-	if (ptr == NULL)
+	index = 0;
+	size = ft_size(str, charset) + 1;
+	p = (char **)malloc(size * sizeof(char *));
+	if (p == NULL)
 		return (NULL);
-	ft_to_arr(ptr, size, str, charset);
-	return (ptr);
+	slen = 0;
+	while (charset[slen])
+		slen++;
+	i = 0;
+	while (i < size - 1)
+	{
+		p[i] = ft_strdup(str, &index, charset, slen);
+		i++;
+	}
+	p[i] = 0;
+	return (p);
 }
